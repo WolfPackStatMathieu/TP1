05efaa8e568d32fd9359ccea45448afa21b7b637
mthomassin <mathieu.thomassin@eleve.ensai.fr>
2022-08-30 09:44:02 +0000

    changement nom dossier

--- business_object/pokemon/abstractPokemon.py
+++ business_object/pokemon/abstractPokemon.py
@@ -1,20 +1,22 @@
 from abc import ABC, abstractmethod
 from statistique import Statistique
 
+
 class AbstractPokemon(ABC):
     """_summary_
 
     Args:
         ABC (_type_): _description_
     """
-    def __init__(self, current_stat, _level, _name):
-        self._current_stat: Statistique = current_stat
+
+    def __init__(self, current_stat: Statistique, _level, _name):
+        self._current_stat = current_stat
         self._level = _level
         self._name = _name
 
     @abstractmethod
     def get_pokemon_attack_coef(self):
         pass
-    
+
     def level_up(self):
-        pass
+        self._level+=1
